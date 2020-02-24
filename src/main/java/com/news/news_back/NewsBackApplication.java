8b745098d3efa93629264fe616869be210971b4f
zhangzulin1234 <1049081426@qq.com>
2020-02-24 15:09:24 +0800

    初始化一个springboot项目

---
+++ src/main/java/com/news/news_back/NewsBackApplication.java
@@ -0,0 +1,13 @@
+package com.news.news_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsBackApplication.class, args);
+	}
+
+}
